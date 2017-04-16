b9c96c299468a09a1f27158234168dc0a5755a26
frosecold <frosecold@gmail.com>
2017-04-16 15:51:26 -0400

    listener mejorado
    pendiente de la cantidad de filas

---
+++ listener1.1.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 13 17:04:46 2017
+
+@author: franco
+"""
+import tweepy
+from tweepy import Stream
+from tweepy import OAuthHandler
+from tweepy.streaming import StreamListener
+import time
+import json
+import sys
+
+#consumer key, consumer secret, access token, access secret.
+ckey="je1LQs78Y7cAaSri9ym6yzbo2"
+csecret="AjOvkFaLul2QVEHbYZIInGoIAgpdXxVNAKmTsRo85qKMoamPu2"
+atoken="120909840-thbApVBzxrfyCZNB2iLBSN2AiZpMTXs6sMgImE6e"
+asecret="tuAJGb79CI0tmpjlhwPcbI2FnLFWSjAy6RXaTcWJhgqVG"
+rows=40000
+iterator=iter(range(rows))
+class listener(StreamListener):
+
+    def on_data(self, data):
+
+        try:
+            global iterator
+            d = json.loads(data)
+            tweet_text= d['text'].replace(","," ")
+            tweet_time= d['created_at']
+            user_id= d['user']['id']
+            user_name= d['user']['name'].replace(","," ")
+            user_screen_name=d['user']['screen_name'].replace(","," ")
+            try:
+                user_location = d['user']['location'].replace(","," ")
+            except:
+                user_location = None
+            LINEA= {"user_name": user_name, 
+                    "user_screen_name": user_screen_name, 
+                    "tweet_text": tweet_text,
+                    "tweet_time": tweet_time,
+                    "user_id": user_id,
+                    "user_location": user_location}
+            try:
+                print(next(iterator))
+            except:
+                pass
+            
+            saveFile = open('PRUEBA.csv', 'a')
+            saveFile.write(str(LINEA))
+            saveFile.write('\n')
+            saveFile.close()
+            #time.sleep(1)
+            return(True)
+        except BaseException as e:
+            print('ERROR: ', str(e))
+            time.sleep(5)
+            pass
+
+    def on_error(self, status):
+        print (status)
+
+auth = OAuthHandler(ckey, csecret)
+auth.set_access_token(atoken, asecret)
+
+twitterStream = Stream(auth, listener())
+twitterStream.filter(track=["trump"])
